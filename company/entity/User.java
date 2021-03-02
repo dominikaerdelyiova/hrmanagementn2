40c15da3a78e2f221b603f82a07dc84519a72944
dominikaerdelyiova <72385168+dominikaerdelyiova@users.noreply.github.com>
2021-03-02 15:21:35 +0100

    Add files via upload

---
+++ company/entity/User.java
@@ -0,0 +1,39 @@
+package sk.kosickaakademia.company.entity;
+
+import sk.kosickaakademia.company.enumerator.Gender;
+
+public class User {
+    private int id;
+    private String fname;
+    private String lname;
+    private int age;
+    private Gender gender;
+
+    public User(int id, String fname, String lname, int age, int gender) {
+        this(fname, lname, age, gender); // volanie konstruktora so 4 parametrami | musi byt na 1. riadku
+        this.id = id;
+    }
+
+    public User(String fname, String lname, int age, int gender) {
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.gender = gender==0?Gender.MALE : gender==1?Gender.FEMALE : Gender.OTHER;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+}
