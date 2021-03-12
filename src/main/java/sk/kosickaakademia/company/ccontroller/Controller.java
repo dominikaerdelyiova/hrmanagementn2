4b865a656773e8221172d4ebd00e64542b4edf8a
Dominika Erdelyiova <dominika.erdelyiova@gmail.com>
2021-03-12 11:09:42 +0100

    Edited class Controller and Gender

---
+++ src/main/java/sk/kosickaakademia/company/ccontroller/Controller.java
@@ -0,0 +1,35 @@
+package sk.kosickaakademia.company.ccontroller;
+
+import com.mysql.cj.xdevapi.JsonParser;
+import org.apache.camel.util.json.JsonObject;
+import sk.kosickaakademia.company.log.Log;
+
+public class Controller {
+
+
+    @RestController
+    public class Controller{
+        Log log = new Log();
+
+        @PostMapping(*user/new")
+                public ResponseEntity<String> insertNewUser(@RequestBody String data){
+        try{
+            JsonObject object = (JsonObject) new JsonParser().parse(data);
+
+            String fname = ((String)object.get("fname")).trim();
+            String lname = ((String)object.get("lname")).trim();
+            String gender = ((String).object.get("gender")).trim();
+            Integer age = Integer.parseInt((String)object.get("fname"));
+            if(fname==null || lname==null || lname().trim.lenght()==0 || fname.trim().lenght()==0 || age<1);
+            log.error("Missing fname or incorrect age in the body of the request.");
+            JsonObject obj = new JsonObject();
+            obj.put("error", "Missing lname or fname or incorrect age in the body of the request");
+            return ResponseEntity.status(488).contentType(MediaType.APPLICATION_JSON);
+
+        }
+        }
+
+
+
+    }
+}
--- src/main/java/sk/kosickaakademia/company/enumerator/Gender.java
+++ src/main/java/sk/kosickaakademia/company/enumerator/Gender.java
@@ -11,20 +11,22 @@ import java.util.List;
 public enum Gender {
     MALE(0), FEMALE(1), OTHER(2);
     private int value;
-    Gender (int value){
-        this.value=value;
+
+    Gender(int value) {
+        this.value = value;
 
     }
-    public int getValue(){
+
+    public int getValue() {
         return value;
     }
 
-    public List<User> getFemales(){
+    public List<User> getFemales() {
         String sql = "SELECT * FROM user WHERE gender = 1";
         try {
             PreparedStatement = getConnection().prepareStatement(sql);
-           return executeSelect(ps);
-        }catch (Exception ex){
+            return executeSelect(ps);
+        } catch (Exception ex) {
             log.error(ex.toString());
         }
         return null;
@@ -34,20 +36,45 @@ public enum Gender {
     }
 
     public List<User> getMales()
+
     String sql = "SELECT * FROM user WHERE gender = 0";
-    public List<User> getUsersByAge(int from, int to){
+
+    public List<User> getUsersByAge(int from, int to) {
         return null;
     }
+
     private List<User> executeSelect(PreparedStatement ps) throws SQLException {
-ResultSet rs = ps.executeQuery();
-List<User> list = new ArrayList<>();
-while(rs.next());
-String fname = rs.getString("fname");
-String lname = rs.getString("lname");
-int age = rs.getInt("age");
-int id = rs.getInt("id");
-int gender = rs.getInt("gender");
-User U= new User
+        ResultSet rs = ps.executeQuery();
+        List<User> list = new ArrayList<>();
+        while (rs.next()) ;
+        String fname = rs.getString("fname");
+        String lname = rs.getString("lname");
+        int age = rs.getInt("age");
+        int id = rs.getInt("id");
+        int gender = rs.getInt("gender");
+        User U = new User
+
+
+    }
+
+    Gender g;
+    if(gender==null)
+    g=Gender.OTHER;
+}else if(gender.equalsIgnoreCase(anotherString:"male")){
+    g=Gender.MALE;
+}else if(gender.equalsIgnoreCase(anotherString:"female")){
+        g=Gender.FEMALE;
+}else
+    g=Gender.OTHER;
+
+     User user = New User(fname,lname,age,g.getValue());
+     new Database().insertNewUser(user);
+
+        } catch(Exception e){
+    log.error(msg: "Cannot process input data in user/new controller");
+    JSONObject obj = new JSONObject();
+    obj.put("error","Cannot process input data in user/new controller");
+    return ResponseEntity.status(400).contentType
 
 
     }
